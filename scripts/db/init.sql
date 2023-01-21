777cfbe9a3c76e3ccdba12ca66fdc6ebabccd682
Matteo Grilla <mgrilla@reach.me>
2023-01-20 19:06:19 -0600

    Add postgres DB

---
+++ scripts/db/init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS functions (
+  "name" TEXT NOT NULL,
+  "env" TEXT NOT NULL,
+  "memory" TEXT NOT NULL,
+  "cpus" TEXT NOT NULL,
+  "type" TEXT NOT NULL,
+
+  PRIMARY KEY ("name")
+);
