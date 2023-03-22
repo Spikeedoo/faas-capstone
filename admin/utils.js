5e51009c2b134526a97906199089081d4cbad0ed
Matteo Grilla <35410141+Spikeedoo@users.noreply.github.com>
2023-03-22 11:53:44 -0500

    MAT-40 initialize cli (#5)
    * Init cli
    
    * Environment and dependency validation
    
    * Setup config command & viper config
    
    * Deploy from CLI

--- admin/utils.js
+++ admin/utils.js
@@ -40,9 +40,41 @@ const resolveUploadPath = (file) => {
   return path.resolve('upload', file)
 }
 
+const fileExists = async (path) => {  
+  try {
+    await fs.access(path)
+    return true
+  } catch {
+    return false
+  }
+}
+
+// Constants
+const ENV_NODE_14_CONFIG = {
+  name: 'node-14',
+  depFilePath: 'package.json'
+}
+
+const ENV_PYTHON3_CONFIG = {
+  name: 'python3',
+  depFilePath: 'requirements.txt',
+}
+
+const VALID_ENV_CONFIGS = [ENV_NODE_14_CONFIG, ENV_PYTHON3_CONFIG]
+const VALID_ENV_NAMES = VALID_ENV_CONFIGS.map(config => config.name)
+
+const VALID_MEMORY_CONFIGS = ["128", "256", "512", "1024", "2048", "4096", "8192"]
+const VALID_CPU_CONFIGS = ["1", "2", "4"]
+
 module.exports = {
   copyFiles,
   listFilesRecursively,
   resolveBuildPath,
   resolveUploadPath,
+  fileExists,
+
+  VALID_ENV_CONFIGS,
+  VALID_ENV_NAMES,
+  VALID_MEMORY_CONFIGS,
+  VALID_CPU_CONFIGS,
 }
---
+++ cli/cmd/root.go
@@ -0,0 +1,217 @@
+package cmd
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"os/exec"
+	"strconv"
+	"time"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+const CONFIG_REMOTE_SERVER string = "CONFIG_REMOTE_SERVER"
+
+// Types
+type DEPLOY_RESPONSE struct {
+	Success bool   `json:"success"`
+	Error   string `json:"error"`
+}
+
+// Helper functions
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func parseJson(body io.ReadCloser, target interface{}) error {
+	return json.NewDecoder(body).Decode(target)
+}
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "faas",
+	Short: "Used to manage FaaS deployment",
+	Long:  `Command line tool to interact with a FaaS deployment`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// *** This command is used to deploy a function to the remote server *** //
+var deployCmd = &cobra.Command{
+	Use:   "deploy",
+	Short: "Deploy a cloud function",
+	Long:  `Used to deploy a cloud function--either to create or update it.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// Verify that a build dir has been created
+		homedir, err := os.UserHomeDir()
+		check(err)
+
+		builddir := homedir + "/.faascli/builds/"
+		buildDirErr := os.MkdirAll(builddir, os.ModePerm)
+		check(buildDirErr)
+
+		// Verify that a server has been set
+		serverUrl := viper.Get(CONFIG_REMOTE_SERVER)
+		// Verify that an env has been passed
+		if serverUrl == nil {
+			fmt.Println("You have not configured a remote server!\nPlease use faas config --server <IP>")
+		} else if !cmd.Flags().Lookup("env").Changed {
+			fmt.Println("Missing a target environment!\nPlease add the --env <envname> flag!")
+		} else if !cmd.Flags().Lookup("name").Changed {
+			fmt.Println("Missing a target function name!\nPlease add the --name <function name> flag!")
+		} else if !cmd.Flags().Lookup("module").Changed {
+			fmt.Println("Missing a module path!\nPlease add the --module <path> flag!")
+		} else {
+			fmt.Println("Staring function deploy...")
+
+			env, _ := cmd.Flags().GetString("env")
+			name, _ := cmd.Flags().GetString("name")
+			module, _ := cmd.Flags().GetString("module")
+
+			httpClient := http.Client{}
+
+			tarballName := builddir + "build_" + strconv.FormatInt(time.Now().Unix(), 10) + ".tar"
+			_, err := exec.Command("tar", "cf", tarballName, ".").Output()
+			check(err)
+			// Step 2: Call the /deploy endpoint with the tarred directory
+			var (
+				buf = new(bytes.Buffer)
+				w   = multipart.NewWriter(buf)
+			)
+
+			// Pass file to request
+			tarballFileContents, err := os.ReadFile(tarballName)
+			check(err)
+
+			part, err := w.CreateFormFile("deployment", tarballName)
+			check(err)
+
+			_, writeErr := part.Write(tarballFileContents)
+			check(writeErr)
+
+			w.WriteField("env", env)
+			w.WriteField("functionName", name)
+			w.WriteField("module", module)
+
+			closeErr := w.Close()
+			check(closeErr)
+
+			// Fire deploy request
+			deployUrl := serverUrl.(string) + "/api/cloudfunctions/deploy"
+			deployReq, err := http.NewRequest("POST", deployUrl, buf)
+			check(err)
+
+			deployReq.Header.Add("Content-Type", w.FormDataContentType())
+
+			res, err := httpClient.Do(deployReq)
+			check(err)
+			defer res.Body.Close()
+
+			deployRes := new(DEPLOY_RESPONSE)
+			parseJson(res.Body, deployRes)
+			buildErr := deployRes.Error
+			buildSuccess := deployRes.Success
+			if buildErr != "" {
+				if buildErr != "" {
+					fmt.Println(buildErr)
+				}
+				fmt.Println("An unknown error happened during deploy!")
+				return
+			}
+
+			if buildSuccess {
+				fmt.Println("Function '" + name + "' successfully deployed!")
+				return
+			}
+		}
+	},
+}
+
+// *** This command is used to set config variables for the CLI to use *** //
+var configCmd = &cobra.Command{
+	Use:   "config",
+	Short: "Configure FaaS settings",
+	Long:  `Used to set configuration values for FaaS.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		server, _ := cmd.Flags().GetString("server")
+		if server != "" {
+			viper.Set(CONFIG_REMOTE_SERVER, server)
+			err := viper.WriteConfig()
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Println("Remote server set:", server)
+			}
+		} else {
+			fmt.Println("")
+		}
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.faascli.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	configCmd.Flags().String("server", "s", "")
+
+	deployCmd.Flags().String("env", "e", "")
+	deployCmd.Flags().String("name", "n", "")
+	deployCmd.Flags().String("module", "m", "")
+
+	rootCmd.AddCommand(deployCmd)
+	rootCmd.AddCommand(configCmd)
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".faascli" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".faascli")
+		viper.SafeWriteConfig()
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		// fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
